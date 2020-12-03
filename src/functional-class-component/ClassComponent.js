9459bce027e0709a920f34277e931894f20f08f3
denandra prasetya <denandra1628@gmail.com>
2020-12-03 17:28:24 +0700

    functional and class component

---
+++ src/functional-class-component/ClassComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class ClassComponent extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Hello, Class Component! </Text>
+            </View>
+        )
+    }
+}
---
+++ src/functional-class-component/FunctionalComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const FunctionalComponent = () => {
+  return (
+      <Text>Hello, Functional Component!</Text>
+  );
+}
+
+export default FunctionalComponent;
